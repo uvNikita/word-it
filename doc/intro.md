b05609f6f309f334ef39e054046390fec06bba44
Nikita Uvarov <uv.nikita@gmail.com>
2016-02-25 18:42:44 +0100

    Initial leiningen project bootstrap

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to word-it
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/word_it/core_test.clj
@@ -0,0 +1,7 @@
+(ns word-it.core-test
+  (:require [clojure.test :refer :all]
+            [word-it.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
