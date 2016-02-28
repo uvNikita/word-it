b60be3b668b54e2e833c09ae846f82f7693e1f6b
Nikita Uvarov <uv.nikita@gmail.com>
2016-02-28 23:13:47 +0100

    Add possibility of multiple translations
    Different translations of the word should be separated using comma (',')

--- src/word_it/core.clj
+++ src/word_it/core.clj
@@ -3,35 +3,39 @@
 
 (require '[clojure.string :as str])
 
-(defn read-words
+(defn read-dict
   [filename]
   (with-open [rdr (clojure.java.io/reader filename)]
     (let [trans (->> (line-seq rdr)
                      (map str/trim)
                      (filter not-empty)
                      (filter #(not (.startsWith % "#")))
-                     (map #(str/split % #" - ")))]
+                     (map #(str/split % #" - "))
+                     (map (fn [line]
+                            (->> line
+                                 (map #(str/split % #","))
+                                 (map #(map str/trim %))))))]
       (into [] trans))))
 
 
 (defn ask
   [dicts lang]
-  (let [[word, translation] (rand-nth (dicts lang))
+  (let [[words, translations] (rand-nth (dicts lang))
         [word-lang-name, trans-lang-name] (case lang
                                             :norsk ["norsk", "eng"]
                                             :eng ["eng", "norsk"])]
-    (println (str word-lang-name, ":\t", word))
+    (println (str word-lang-name, ":\t", (str/join ", " words)))
     (let [answer (do (print (str trans-lang-name, ":\t"))
                      (flush)
                      (str/trim (read-line)))]
       (cond
-        (= answer translation) :correct
+        (some #(= answer %) translations) :correct
         (= answer "quit") :quit
-        :else (do (println translation) :wrong)))))
+        :else (do (println (str/join ", " translations)) :wrong)))))
 
 
 (defn -main [filename & args]
-  (let [norsk-eng (read-words filename)
+  (let [norsk-eng (read-dict filename)
         dicts {:norsk norsk-eng
                :eng   (map (fn [[norsk, eng]] [eng, norsk]) norsk-eng)}
         total (atom 0)
